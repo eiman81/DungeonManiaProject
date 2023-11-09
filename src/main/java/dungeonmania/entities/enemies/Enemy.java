da538703712ad866390f8719559116cb5da116cf
Minjun Kim <z5421922@ad.unsw.edu.au>
2023-11-09 15:18:19 +1100

    implemented 1c: inheritance code smell

--- src/main/java/dungeonmania/entities/enemies/Enemy.java
+++ src/main/java/dungeonmania/entities/enemies/Enemy.java
@@ -43,11 +43,6 @@ public abstract class Enemy extends Entity implements Battleable {
         g.unsubscribe(getId());
     }
 
-    @Override
-    public void onMovedAway(GameMap map, Entity entity) {
-        return;
-    }
-
     /**
      * set the movement strategy for the enemy
      * @param movementBehaviour
