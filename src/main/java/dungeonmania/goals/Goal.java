2657492797910df5032bec0b7597babb737dcdd5
Eiman <z5417758@ad.unsw.edu.au>
2023-11-10 20:08:42 +1100

    Fixed complex goals not working by changing goal.java + removing buildable import lint

--- src/main/java/dungeonmania/goals/Goal.java
+++ src/main/java/dungeonmania/goals/Goal.java
@@ -4,27 +4,37 @@ import dungeonmania.Game;
 
 public class Goal {
     private GoalStrategy strategy;
+    private String type;
+    private int target;
+    private Goal goal1;
+    private Goal goal2;
 
     public Goal(String type) {
+        this.type = type;
+    }
+
+    public Goal(String type, int target) {
+        this.type = type;
+        this.target = target;
+    }
+
+    public Goal(String type, Goal goal1, Goal goal2) {
+        this.type = type;
+        this.goal1 = goal1;
+        this.goal2 = goal2;
+    }
+
+    /**
+     * Picks the correct strategy based on the type of the goal
+     */
+    private void pickGoalStrategy() {
         if (type.equals("exit"))
             strategy = new ExitGoal();
         else if (type.equals("boulders"))
             strategy = new BouldersGoal();
-        else {
-            strategy = new DefaultGoal();
-        }
-    }
-
-    public Goal(String type, int target) {
-        if (type.equals("treasure"))
+        else if (type.equals("treasure"))
             strategy = new TreasureGoal(target);
-        else {
-            strategy = new DefaultGoal();
-        }
-    }
-
-    public Goal(String type, Goal goal1, Goal goal2) {
-        if (type.equals("AND"))
+        else if (type.equals("AND"))
             strategy = new AndGoal(goal1, goal2);
         else if (type.equals("OR"))
             strategy = new OrGoal(goal1, goal2);
@@ -34,10 +44,16 @@ public class Goal {
     }
 
     public boolean achieved(Game game) {
+        if (game.getPlayer() == null)
+            return false;
+        pickGoalStrategy();
         return strategy.achieved(game);
     }
 
     public String toString(Game game) {
+        pickGoalStrategy();
+        if (this.achieved(game))
+            return "";
         return strategy.toString(game);
     }
 }
