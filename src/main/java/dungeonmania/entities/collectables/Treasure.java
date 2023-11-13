b257c9fe6d8c2c348c18d5c47710a7c0ca193b59
Minjun Kim <z5421922@ad.unsw.edu.au>
2023-11-13 14:20:50 +1100

    fix treasure for treasure

--- src/main/java/dungeonmania/entities/collectables/Treasure.java
+++ src/main/java/dungeonmania/entities/collectables/Treasure.java
@@ -6,7 +6,7 @@ import dungeonmania.entities.inventory.InventoryItem;
 import dungeonmania.map.GameMap;
 import dungeonmania.util.Position;
 
-public class Treasure extends Entity implements InventoryItem {
+public class Treasure extends Collectables implements InventoryItem {
     public Treasure(Position position) {
         super(position);
     }
