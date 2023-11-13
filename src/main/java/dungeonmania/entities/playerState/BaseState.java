3feb84d9742355bcb7038995ce201b89f9f32579
Minjun Kim <z5421922@ad.unsw.edu.au>
2023-11-13 14:01:54 +1100

    refactor 1f: playerStates

--- src/main/java/dungeonmania/entities/playerState/BaseState.java
+++ src/main/java/dungeonmania/entities/playerState/BaseState.java
@@ -1,26 +1,15 @@
 package dungeonmania.entities.playerState;
 
+import dungeonmania.battles.BattleStatistics;
 import dungeonmania.entities.Player;
 
 public class BaseState extends PlayerState {
     public BaseState(Player player) {
-        super(player, false, false);
+        super(player);
     }
 
     @Override
-    public void transitionBase() {
-        // Do nothing
-    }
-
-    @Override
-    public void transitionInvincible() {
-        Player player = getPlayer();
-        player.changeState(new InvincibleState(player));
-    }
-
-    @Override
-    public void transitionInvisible() {
-        Player player = getPlayer();
-        player.changeState(new InvisibleState(player));
+    public BattleStatistics applyBuff(BattleStatistics origin) {
+        return origin;
     }
 }
